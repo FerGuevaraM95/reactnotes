843ce9b6b3a8fd927ce0211b5e848d45a63cae44
Fernando Guevara Marquez <fernandoguevaramarquez@MacBook-Pro-de-Fernando.local>
2017-12-29 12:25:19 -0600

    Commit inicial

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyAg6Ok_tFEDbqOcV7RxB2kxCC7LcI4OWno",
+  authDomain: "notesreact-20fb0.firebaseapp.com",
+  databaseURL: "https://notesreact-20fb0.firebaseio.com",
+  projectId: "notesreact-20fb0",
+  storageBucket: "notesreact-20fb0.appspot.com",
+  messagingSenderId: "294635930818"
+}
